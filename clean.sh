a3f49934e39b1ebb235263f459f7e6398af5ee63
Ming-der Wang <ming@bimap.co>
2019-12-22 17:00:44 +0800

    add gen node cert and clean.sh

---
+++ clean.sh
@@ -0,0 +1,4 @@
+rm admin-key-temp.pem
+rm admin.csr
+rm node-key-temp.pem
+rm node.csr
---
+++ genNodeCert.sh
@@ -0,0 +1,6 @@
+# Node cert
+openssl genrsa -out node-key-temp.pem 2048
+openssl pkcs8 -inform PEM -outform PEM -in node-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out node-key.pem
+openssl req -new -key node-key.pem -out node.csr
+openssl x509 -req -in node.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out node.pem
+
