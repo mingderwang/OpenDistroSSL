d2b0ac332943620f154bfdbf3820310187636607
Ming-der Wang <ming@bimap.co>
2019-12-22 16:56:47 +0800

    use gen2.sh to gen admin.pem

---
+++ gen2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+openssl genrsa -out admin-key-temp.pem 2048
+openssl pkcs8 -inform PEM -outform PEM -in admin-key-temp.pem -topk8 -nocrypt -v1 PBE-SHA1-3DES -out admin-key.pem
+openssl req -new -key admin-key.pem -out admin.csr -subj "/C=TW/ST=Taiwan/L=Taipei City/O=Bimap/OU=RD/CN=Ming-der Wang"
+openssl x509 -req -in admin.csr -CA root-ca.pem -CAkey root-ca-key.pem -CAcreateserial -sha256 -out admin.pem
